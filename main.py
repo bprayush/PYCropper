c37ad9bf539f2a2ce624c9a202fec0c0495efa79
bprayush <bprayush@yahoo.com>
2019-12-06 14:17:35 +0545

    PYCropper for image scrapper

---
+++ main.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python3
+
+from PIL import Image
+
+image = Image.open("Pokemon.jpg")
+
+# maxHeight, maxWidth = image.
+cropWidth, cropHeight = (250, 250)
+cropBounds = (0, 0, cropWidth, cropHeight)
+
+horizontalNumber = 16
+verticalNumber = 10
+
+backgroundColor = (250, 250, 250)
+
+path = "exported/"
+spriteFolder = "pokemon/"
+
+counter = 1
+
+for left in range(0, cropWidth * horizontalNumber, cropWidth):
+    for upper in range(0, cropHeight * verticalNumber, cropHeight):
+        cropBounds = (left, upper, left+cropWidth, upper+cropHeight)
+        resized = image.crop(cropBounds)
+        resized = resized.convert("RGBA")
+
+        data = resized.getdata()
+        # print(data)
+        temp = list()
+        for r,g,b,a in data:
+            # print(r,g,b,a)
+            if r >= backgroundColor[0] and g >= backgroundColor[1] and b >= backgroundColor[2]:
+                temp.append((r,g,b,0))
+                continue
+            temp.append((r,g,b,a))
+        resized.putdata(temp)
+        resized.save(path + spriteFolder +str(counter)+".png", "PNG")
+        counter = counter + 1
+image.close()
